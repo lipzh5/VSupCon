b7b2f07773e7586cd4e01c30f2b8f970d4e1bd09
penny <2567211251@qq.com>
2024-09-05 22:41:22 +1000

    modify data path

--- datasets/affwild2.py
+++ datasets/affwild2.py
@@ -107,7 +107,7 @@ cwd = osp.abspath(osp.dirname(__file__))
 def get_affwild2_dataset(split, transform_ops, downsampling=1):
     root_path = osp.join(cwd, '../../common/data/aff-wild2') # '/home/penny/pycharmprojects/common/data/aff-wild2'
     data_list_dir = osp.join(root_path, 'preprocessed_data')
-    # data_folder = osp.join(root_path, 'cropped_all/cropped_aligned')
-    data_folder = osp.join(root_path, 'openface_masked_cropped')
+    data_folder = osp.join(root_path, 'cropped_all/cropped_aligned')
+    # data_folder = osp.join(root_path, 'openface_masked_cropped')
     anno_folder = osp.join(root_path, '5th_ABAW_Annotations/EXPR_Classification_Challenge')
     return AffwildDataset(data_list_dir, data_folder, anno_folder, split, transform_ops, downsampling)
